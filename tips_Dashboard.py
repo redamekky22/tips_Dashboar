be2e8a38e48c85d102679879837fb78424690f04
redamekky22 <reda.mekky22@gmail.com>
2024-06-17 15:38:07 +0300

    Update tips_Dashboard.py

--- tips_Dashboard.py
+++ tips_Dashboard.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 # must be first commend 
